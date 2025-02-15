a48ee4f89f42cca09d96f3b0ffb3288d1dd81c7f
mfajrihusaini02 <mfajrihusaini05@gmail.com>
2025-02-15 17:45:31 +0700

    Convert Cart Page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DetailController;
 use App\Http\Controllers\HomeController;
@@ -19,5 +20,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/categories', [CategoryController::class, 'index'])->name('categories');
 Route::get('/details/{id}', [DetailController::class, 'index'])->name('detail');
+Route::get('/cart', [CartController::class, 'index'])->name('cart');
 
 // Auth::routes();
