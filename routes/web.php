d11e58ee1da83e9f03feba94c2d65c55f57f5d4d
mfajrihusaini02 <mfajrihusaini05@gmail.com>
2025-02-20 22:52:11 +0700

    Create Template Admin

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\CategoryController;
@@ -42,5 +43,12 @@ Route::get('/dashboard/transactions/{id}', [DashboardTransactionController::clas
 Route::get('/dashboard/settings', [DashboardSettingController::class, 'store'])->name('dashboard-setting-store');
 
 Route::get('/dashboard/account', [DashboardSettingController::class, 'account'])->name('dashboard-setting-account');
+
+Route::prefix('admin')
+    ->namespace('Admin')
+    ->group(function () {
+        Route::get('/', [AdminDashboardController::class, 'index'])->name('admin-dashboard');
+    });
+// ->middleware(['auth', 'admin'])
 
 Auth::routes();
