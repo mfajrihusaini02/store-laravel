a67ee6beb85f311fbe26034b67f07d5d11766678
mfajrihusaini02 <mfajrihusaini05@gmail.com>
2025-03-01 00:05:46 +0700

    Update code

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -92,7 +92,7 @@
                         </div>
                     @empty
                         <div class="col-12 text-center py-5" data-aos="fade-up" data-aos-delay="100">
-                            No Categories Found
+                            Not Found
                         </div>
                     @endforelse
                 </div>
