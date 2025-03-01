5e062e7eb7575ca0b0ad0c02ea612da67fe054df
mfajrihusaini02 <mfajrihusaini05@gmail.com>
2025-03-01 22:55:29 +0700

    Integrasi Login Page

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -23,4 +23,16 @@ class CategoryController extends Controller
             'products' => $products,
         ]);
     }
+
+    public function detail(Request $request, $slug)
+    {
+        $categories = Category::all();
+        $category = Category::where('slug', $slug)->firstOrFail();
+        $products = Product::with(['galleries'])->where('categories_id', $category->id)->paginate(32);
+
+        return view('pages.category', [
+            'categories' => $categories,
+            'products' => $products,
+        ]);
+    }
 }
--- app/Http/Controllers/DetailController.php
+++ app/Http/Controllers/DetailController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class DetailController extends Controller
@@ -11,8 +13,27 @@ class DetailController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Request $request, $slug)
     {
-        return view('pages.detail');
+        $categories = Category::all();
+        $category = Category::where('slug', $slug)->firstOrFail();
+        $products = Product::with(['galleries'])->where('categories_id', $category->id)->paginate(32);
+
+        return view('pages.detail', [
+            'categories' => $categories,
+            'products' => $products,
+        ]);
+    }
+
+    public function detail(Request $request, $slug)
+    {
+        $categories = Category::all();
+        $category = Category::where('slug', $slug)->firstOrFail();
+        $products = Product::with(['galleries'])->where('categories_id', $category->id)->paginate(32);
+
+        return view('pages.detail', [
+            'categories' => $categories,
+            'products' => $products,
+        ]);
     }
 }
