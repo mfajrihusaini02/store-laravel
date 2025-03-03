a21eafbc8703703d44e0702a6ccd4b0ec90d57ab
mfajrihusaini02 <mfajrihusaini05@gmail.com>
2025-03-03 14:04:49 +0700

    Integrasi data Detail Page

--- app/Http/Controllers/DetailController.php
+++ app/Http/Controllers/DetailController.php
@@ -15,24 +15,9 @@ class DetailController extends Controller
      */
     public function index(Request $request, $slug)
     {
-        $categories = Category::all();
-        $category = Category::where('slug', $slug)->firstOrFail();
-        $products = Product::with(['galleries'])->where('categories_id', $category->id)->paginate(32);
+        $products = Product::with(['galleries', 'user'])->where('slug', $slug)->firstOrFail();
 
         return view('pages.detail', [
-            'categories' => $categories,
-            'products' => $products,
-        ]);
-    }
-
-    public function detail(Request $request, $slug)
-    {
-        $categories = Category::all();
-        $category = Category::where('slug', $slug)->firstOrFail();
-        $products = Product::with(['galleries'])->where('categories_id', $category->id)->paginate(32);
-
-        return view('pages.detail', [
-            'categories' => $categories,
             'products' => $products,
         ]);
     }
