681725bf63d4456f51536b3ac9a1b99ef1739a48
mfajrihusaini02 <mfajrihusaini05@gmail.com>
2025-03-06 17:58:23 +0700

    Integrasi Dashboard - Update Model Transaction Detail

--- app/Models/TransactionDetail.php
+++ app/Models/TransactionDetail.php
@@ -29,4 +29,14 @@ class TransactionDetail extends Model
      * @var array<int, string>
      */
     protected $hidden = [];
+
+    public function product()
+    {
+        return $this->hasOne(Product::class, 'id', 'products_id');
+    }
+
+    public function transaction()
+    {
+        return $this->hasOne(Transaction::class, 'id', 'transactions_id');
+    }
 }
